d7cab2e0ca7f04810dd41bfe8d6e81b449f3b129
laainra <lailaar77@gmail.com>
2024-09-23 13:15:25 +0700

    feat: change some db model

--- Website/Dashboard/models.py
+++ Website/Dashboard/models.py
@@ -1,19 +1,27 @@
 from django.db import models
 
+
 class Personnels(models.Model):
     genders = {
-        "M":"Male",
-        "F":"Female",
+        "M": "Male",
+        "F": "Female",
+    }
+    roles = {
+        "S": "Staff",
+        "I": "Intern",
+        "U": "Unknown",
     }
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
+    employment_status = models.CharField(max_length=100, choices=roles)
     gender = models.CharField(max_length=100, choices=genders)
-    createdAt = models.DateTimeField(auto_now_add=True,null=True)
-    updatedAt = models.DateTimeField(auto_now=True,null=True)
+    createdAt = models.DateTimeField(auto_now_add=True, null=True)
+    updatedAt = models.DateTimeField(auto_now=True, null=True)
+
 
 class Camera_Settings(models.Model):
     id = models.AutoField(primary_key=True)
-    cam_name = models.CharField(max_length=200) 
+    cam_name = models.CharField(max_length=200)
     feed_src = models.CharField(max_length=200)
     x1 = models.IntegerField(default=0)
     y1 = models.IntegerField(default=0)
@@ -36,8 +44,12 @@ class Camera_Settings(models.Model):
     face_detection = models.BooleanField(default=False)
     cam_start = models.CharField(max_length=200)
     cam_stop = models.CharField(max_length=200)
+    attendance_time_start = models.CharField(max_length=200)
+    attendance_time_end = models.CharField(max_length=200)
+    leaving_time_start = models.CharField(max_length=200)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
+
 
 class Counted_Instances(models.Model):
     id = models.AutoField(primary_key=True)
@@ -49,8 +61,11 @@ class Counted_Instances(models.Model):
     people_exits = models.IntegerField(default=0)
     people_inside = models.IntegerField(default=0)
 
+
 class Personnel_Entries(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     timestamp = models.CharField(max_length=200)
+    time_in = models.CharField(max_length=200)
+    time_out = models.CharField(max_length=200)
     camera = models.ForeignKey(Camera_Settings, on_delete=models.CASCADE)
